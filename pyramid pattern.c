99577703c4adda584733b1ad2767e440a25af90c
LeelaMohan Mannem <81023420+Mohan123451@users.noreply.github.com>
2021-03-21 07:57:17 +0530

    Add files via upload

---
+++ pyramid pattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+    int num,row,col;
+    scanf("%d",&num);
+    for (row=1; row<=num; row++)
+    {
+    for (col=1; col<num-row; col++)
+    {
+        printf(" ");
+    }
+    for (col=1; col<=2*row-1; col++)
+    {
+        printf("*");
+    }
+    for (col=1; col<=num-row; col++)
+    {
+        printf(" ");
+    }
+        printf("\n");
+    }
+    return 0;
+}
